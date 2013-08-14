fab28ab577fe281f4b913a80f2a6e7c7e5533dbb
itraa <itraa.narayan@gmail.com>
2013-08-14 12:50:21 -0400

    Sprint2 submission

--- chap6.py
+++ chap6.py
@@ -1,9 +1,38 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Aarthi Narayan
 
 
 # Ex. 6.6
+def first(word):
+    return word[0]
 
+def last(word):
+    return word[-1]
 
+def middle(word):
+    return word[1:-1]
 
+def is_palindrome(word):
+    
+    if len(word) <= 1: 
+        return True
+
+    if first(word) == last(word):
+        return is_palindrome(middle(word))
+    else:    
+        return False    
+        
+
+print is_palindrome('redivider')
+print is_palindrome('neon')
 # Ex 6.8
+
+def gcd(a,b):
+
+    if b == 0:
+        return a
+    else:    
+        return gcd(b,a % b)
+
+
+print gcd(12,15)
--- chap7.py
+++ chap7.py
@@ -1,9 +1,28 @@
 # Enter your answrs for chapter 7 here
-# Name:
-
+# Name:Aarthi Narayan
+import math
 
 # Ex. 7.5
 
+def fact(k):
+    if k == 0:
+        return 1
+    else:
+        return k * fact(k-1)
 
+def estimate_pi():
+    k = 0
+    summation = 0
+    while True:
+        sum =  (fact(4 * k) * (1103 + 26390 * k)) / ((fact(k)**4) * 396**(4*k)) 
+        summation = sum + summation
+        if sum < 1e-15
+            break
+        k = k + 1
 
+    return 1/((2 * math.sqrt(2)/9801) * summation)
+
+print estimate_pi()
+#print math.pi
 # How many iterations does it take to converge?
+Two iterations
